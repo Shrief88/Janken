94055591eb039b5654c627305458cf711ab8f52c
Shrief Essam <shriefessam1999@gmail.com>
2022-04-11 00:46:10 +0200

    adding singleRound function The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round

--- external.js
+++ external.js
@@ -3,3 +3,27 @@ function computerPlay(){
     return options[Math.floor(Math.random() * options.length)];
 }
 
+function singleRound(playerSelection,computerSelection){
+    if(playerSelection == 'rock' && computerSelection == 'scissors'){
+        return "You Win! Rock beats Scissors";
+    }
+    else if (playerSelection == 'scissors'&& computerSelection == 'paper' ){
+        return "You Win! Scissors beats Paper";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        return "You Win! Paper beats Rock"
+    }
+    else if(computerSelection == 'rock' && playerSelection == 'scissors'){
+        return "You Lose! Rock beats Scissors";
+    }
+    else if(computerSelection == 'scissors'&& playerSelection == 'paper' ){
+        return "You Lose! Scissors beats Paper";
+    }
+    else if(computerSelection == 'paper' && playerSelection == 'rock'){
+        return "You Lose! Paper beats Rock"
+    }
+    else if (playerSelection == computerSelection){
+        return 'It is a Draw'
+    }
+}
+
