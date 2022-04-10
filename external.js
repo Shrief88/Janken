f6c4b0bdba9cf297d4706f682fc4a9d91133977a
Shrief Essam <shriefessam1999@gmail.com>
2022-04-11 01:05:02 +0200

    modifying the singleRound function It is now returning a value that shows who is the winner

--- external.js
+++ external.js
@@ -3,27 +3,41 @@ function computerPlay(){
     return options[Math.floor(Math.random() * options.length)];
 }
 
+
 function singleRound(playerSelection,computerSelection){
+    const Results = ['win','lose','Draw']
+     
     if(playerSelection == 'rock' && computerSelection == 'scissors'){
-        return "You Win! Rock beats Scissors";
+        console.log('"You Win! Rock beats Scissors"');
+        return Results[0];
     }
     else if (playerSelection == 'scissors'&& computerSelection == 'paper' ){
-        return "You Win! Scissors beats Paper";
+        console.log('"You Win! Scissors beats Paper"');
+        return Results[0];
     }
     else if (playerSelection == 'paper' && computerSelection == 'rock'){
-        return "You Win! Paper beats Rock"
+        console.log('You Win! Paper beats Rock');
+        return Results[0];
     }
     else if(computerSelection == 'rock' && playerSelection == 'scissors'){
-        return "You Lose! Rock beats Scissors";
+        console.log('You Lose! Rock beats Scissors');
+        return Results[1];
     }
     else if(computerSelection == 'scissors'&& playerSelection == 'paper' ){
-        return "You Lose! Scissors beats Paper";
+        console.log('You Lose! Scissors beats Paper');
+        return Results[1];
     }
     else if(computerSelection == 'paper' && playerSelection == 'rock'){
-        return "You Lose! Paper beats Rock"
+        console.log('You Lose! Paper beats Rock');
+        return Results[1];
     }
     else if (playerSelection == computerSelection){
-        return 'It is a Draw'
+        console.log('It is a Draw');
+        return Results[2];
     }
 }
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(singleRound(playerSelection, computerSelection));
 
