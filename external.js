78dd93ab01ba13cda922ce0757c80f031bf32eef
Shrief Essam <shriefessam1999@gmail.com>
2022-04-17 20:29:10 +0200

    add button to reset the game

--- external.js
+++ external.js
@@ -3,9 +3,6 @@ function computerPlay(){
     return options[Math.floor(Math.random() * options.length)];
 }
 
-
-
-
 function singleRound(playerSelection,computerSelection){
     const Results = ['win','lose','Draw']
      
@@ -20,35 +17,54 @@ function singleRound(playerSelection,computerSelection){
     }
 }
 
+function reset(playerScore,computerScore){
+    playerScore.textContent = 0;
+    computerScore.textContent = 0;
+
+}
+
 
 
 const buttons = document.querySelectorAll('.button');
-    buttons.forEach((button)=>{ 
-        button.addEventListener('click',function(e){
-            let playerSelection = button.getAttribute('data-option');
-            const roundResult = document.querySelector('#roundResult');
-            const playerScore = document.querySelector('.playerScore h1');
-            const computerScore = document.querySelector('.computerScore h1');
-            computerSelection = computerPlay();
-            let result = (singleRound(playerSelection,computerSelection));
-
-            if(result == 'win'){
-                playerScore.textContent++;
-                roundResult.textContent = `You Win, ${playerSelection} beats ${computerSelection}`;
-            }
-            else if (result == 'lose'){
-                computerScore.textContent++;
-                roundResult.textContent = `You Lose, ${computerSelection} beats ${playerSelection}`
-            }
-            else {
-                roundResult.textContent = 'It is a Draw';
-            }
+const playerScore = document.querySelector('.playerScore h1');
+const computerScore = document.querySelector('.computerScore h1');
+const resetButton = document.querySelector('#reset');
 
 
+buttons.forEach((button)=>{ 
+    button.addEventListener('click',function(e){
+        let playerSelection = button.getAttribute('data-option');
+        const roundResult = document.querySelector('#roundResult');
+        computerSelection = computerPlay();
+        let result = (singleRound(playerSelection,computerSelection));
 
-
-        })
+        if(result == 'win'){
+            playerScore.textContent++;
+            roundResult.textContent = `You Win, ${playerSelection} beats ${computerSelection}`;
+        }
+        else if (result == 'lose'){
+            computerScore.textContent++;
+            roundResult.textContent = `You Lose, ${computerSelection} beats ${playerSelection}`
+        }
+        else {
+            roundResult.textContent = 'It is a Draw';
+        }
     })
+})
+
+resetButton.addEventListener('click', ()=>{
+    reset(playerScore,computerScore);
+});
+ 
+
+
+
+
+
+
+
+
+    
     
 
 
